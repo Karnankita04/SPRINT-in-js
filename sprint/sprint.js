888232a2f6ea10bbb598e6221251c9297a5b167e
Karnankita04 <91003709+Karnankita04@users.noreply.github.com>
2024-12-29 15:51:03 +0530

    added jumpIfEqual and jumpIfLessThan

--- sprint/sprint.js
+++ sprint/sprint.js
@@ -1,47 +1,68 @@
 console.log("Welcome to my Sprint\n");
 
-const code = prompt("Enter your series: ").split(" ").map((element) => +element );
+const code = prompt("Enter your series: ")
+  .split(" ")
+  .map((element) => +element);
 
 const subtract = (loc) => {
-  const locOfSubtractWith = code[loc + 1]; //code[3]
-  const locOfSubtractTo = code[loc + 2]; //code[4]
-  const locOfResult = code[loc + 3]; //code[2]
+  const locOfSubtractWith = code[loc + 1];
+  const locOfSubtractTo = code[loc + 2];
+  const locOfResult = code[loc + 3];
 
   code[locOfResult] = code[locOfSubtractWith] - code[locOfSubtractTo];
   return loc + 4;
 };
 
 const add = (loc) => {
-  const locOfAddWith = code[loc + 1]; //code[3]
-  const locOfAddTo = code[loc + 2]; //code[4]
-  const locOfResult = code[loc + 3]; //code[2]
+  const locOfAddWith = code[loc + 1];
+  const locOfAddTo = code[loc + 2];
+  const locOfResult = code[loc + 3];
 
-  code[locOfResult] = code[locOfAddWith] + code[locOfAddTo] ;
+  code[locOfResult] = code[locOfAddWith] + code[locOfAddTo];
   return loc + 4;
 };
 
 const jump = (loc) => {
   return loc + 1;
-}
+};
 
-const jumIfEqual = (loc) => {
+const jumpIfEqual = (loc) => {
   const nextLoc = code[loc + 1];
   const nextToNextLoc = code[loc + 2];
 
   if (code[nextLoc] === code[nextToNextLoc]) {
-    return loc + 3;
+    return code[loc + 3];
   }
 
-  return loc + 4;
-}
+  return code[loc + 4];
+};
+
+const jumpIfLessThan = (loc) => {
+  const nextLoc = code[loc + 1];
+  const nextToNextLoc = code[loc + 2];
+
+  if (code[nextLoc] < code[nextToNextLoc]) {
+    return code[loc + 3];
+  }
+
+  return code[loc + 4];
+};
+
+const copy = (loc) => {
+  const nextLoc = code[loc + 1];
+  const nextToNextLoc = code[loc + 2];
+
+  code[nextToNextLoc] = code[nextLoc];
+  return loc + 3;
+};
 
 const instructions = {
   1: add,
   2: subtract,
   3: jump,
-  4: jumIfEqual,
-  // 5: jumpIfLessThan,
-  // 7: copy,
+  4: jumpIfEqual,
+  5: jumpIfLessThan,
+  7: copy,
 };
 
 const instruction = code[1];
@@ -49,10 +70,10 @@ let location = 1;
 
 const executeInstruction = (instruction) => {
   while (instruction !== 9) {
-    // if (instruction in instructions) {
-    location = instructions[instruction](location);
-    instruction = code[location];
-    // }
+    if (instruction in instructions) {
+      location = instructions[instruction](location);
+      instruction = code[location];
+    }
   }
 };
 
