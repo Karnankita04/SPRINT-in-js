61a76da8ddd34dc8b3f77064d69274d55eed0737
Karnankita04 <91003709+Karnankita04@users.noreply.github.com>
2024-12-29 14:55:35 +0530

    added add,subtract,jump,jumpIfEqual instructions

--- sprint/sprint.js
+++ sprint/sprint.js
@@ -1,31 +1,61 @@
-const instruction = { 1: add, 2: subtract, 3: jump, 4: jumIfEqual, 5: jumpIfLessThan, 7: copy };
+console.log("Welcome to my Sprint\n");
 
-const code = prompt("Enter your series: ").split(" ");
+const code = prompt("Enter your series: ").split(" ").map((element) => +element );
 
+const subtract = (loc) => {
+  const locOfSubtractWith = code[loc + 1]; //code[3]
+  const locOfSubtractTo = code[loc + 2]; //code[4]
+  const locOfResult = code[loc + 3]; //code[2]
 
+  code[locOfResult] = code[locOfSubtractWith] - code[locOfSubtractTo];
+  return loc + 4;
+};
 
+const add = (loc) => {
+  const locOfAddWith = code[loc + 1]; //code[3]
+  const locOfAddTo = code[loc + 2]; //code[4]
+  const locOfResult = code[loc + 3]; //code[2]
 
-const add = (loc1, loc2, loc3) => {
+  code[locOfResult] = code[locOfAddWith] + code[locOfAddTo] ;
+  return loc + 4;
+};
 
+const jump = (loc) => {
+  return loc + 1;
 }
 
-const getInput = () => {
-  console.log("Welcome to my Sprint\n");
+const jumIfEqual = (loc) => {
+  const nextLoc = code[loc + 1];
+  const nextToNextLoc = code[loc + 2];
 
-  let index = 0;
+  if (code[nextLoc] === code[nextToNextLoc]) {
+    return loc + 3;
+  }
 
+  return loc + 4;
+}
+
+const instructions = {
+  1: add,
+  2: subtract,
+  3: jump,
+  4: jumIfEqual,
+  // 5: jumpIfLessThan,
+  // 7: copy,
 };
+
+const instruction = code[1];
+let location = 1;
 
 const executeInstruction = (instruction) => {
   while (instruction !== 9) {
-    const arr = code.split(" ").map((number) => +number);
-    console.log(instruction);
-    console.log(arr);
-  
-    if (instruction === 3) {
-    }
-    index++;
-    instruction = arr[index];
+    // if (instruction in instructions) {
+    location = instructions[instruction](location);
+    instruction = code[location];
+    // }
   }
+};
 
-}
+executeInstruction(instruction);
+
+console.log(code);
