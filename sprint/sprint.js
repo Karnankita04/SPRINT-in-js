4134f49fd2dd422c7b93c32496f01bf27f5c52f3
Karnankita04 <91003709+Karnankita04@users.noreply.github.com>
2024-12-29 20:47:51 +0530

    solved bug in jumpIfEqual and jumpIfLessThan

--- sprint/sprint.js
+++ sprint/sprint.js
@@ -1,10 +1,13 @@
 console.log("Welcome to my Sprint\n");
 
-const code = prompt("Enter your series: ")
-  .split(" ")
-  .map((element) => +element);
+const getInput = () => {
+  const code = prompt("Enter your series: ")
+    .split(" ")
+    .map((element) => +element);
+  return code;
+};
 
-const subtract = (loc) => {
+const subtract = (code, loc) => {
   const locOfSubtractWith = code[loc + 1];
   const locOfSubtractTo = code[loc + 2];
   const locOfResult = code[loc + 3];
@@ -13,7 +16,7 @@ const subtract = (loc) => {
   return loc + 4;
 };
 
-const add = (loc) => {
+const add = (code, loc) => {
   const locOfAddWith = code[loc + 1];
   const locOfAddTo = code[loc + 2];
   const locOfResult = code[loc + 3];
@@ -22,11 +25,11 @@ const add = (loc) => {
   return loc + 4;
 };
 
-const jump = (loc) => {
-  return loc + 1;
+const jump = (code, loc) => {
+  return code[loc + 1];
 };
 
-const jumpIfEqual = (loc) => {
+const jumpIfEqual = (code, loc) => {
   const nextLoc = code[loc + 1];
   const nextToNextLoc = code[loc + 2];
 
@@ -34,10 +37,10 @@ const jumpIfEqual = (loc) => {
     return code[loc + 3];
   }
 
-  return code[loc + 4];
+  return loc + 4;
 };
 
-const jumpIfLessThan = (loc) => {
+const jumpIfLessThan = (code, loc) => {
   const nextLoc = code[loc + 1];
   const nextToNextLoc = code[loc + 2];
 
@@ -45,10 +48,10 @@ const jumpIfLessThan = (loc) => {
     return code[loc + 3];
   }
 
-  return code[loc + 4];
+  return loc + 4;
 };
 
-const copy = (loc) => {
+const copy = (code, loc) => {
   const nextLoc = code[loc + 1];
   const nextToNextLoc = code[loc + 2];
 
@@ -65,18 +68,21 @@ const instructions = {
   7: copy,
 };
 
-const instruction = code[1];
-let location = 1;
+const executeInstruction = () => {
+  const code = getInput();
+  let location = 1;
+  let instruction = code[location];
 
-const executeInstruction = (instruction) => {
   while (instruction !== 9) {
     if (instruction in instructions) {
-      location = instructions[instruction](location);
+      location = instructions[instruction](code, location);
       instruction = code[location];
+    } else {
+      return "Invalid Instruction";
     }
   }
+
+  return code;
 };
 
-executeInstruction(instruction);
-
-console.log(code);
+console.log(executeInstruction());
